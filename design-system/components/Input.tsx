a9749f9f526a345298a3322e5fe6e09bb595448c
Hope Tambala <hopetambala@outlook.com>
2020-02-12 02:23:26 -0500

    chore: fix dependencies

--- design-system/components/Input.tsx
+++ design-system/components/Input.tsx
@@ -1,6 +1,6 @@
-import * as React from "react"
-import { theme } from "../theme"
-import styled from "styled-components"
+import * as React from "react";
+import { theme } from "../theme";
+import styled from "styled-components";
 
 // Types
 export type Props = {
