de08d3cb29c10312d71648dbbdfd4cfba9a2ee71
Hope Tambala <hopetambala@outlook.com>
2020-02-21 03:53:38 -0500

    feat: get switches to work!

--- design-system.framerfx/code/canvas.tsx
+++ design-system.framerfx/code/canvas.tsx
@@ -2,7 +2,7 @@
 import { createDesignComponent, CanvasStore } from "framer"
 const canvas = CanvasStore.shared(); // CANVAS_DATA;
 export const Menu = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_N7SPOhiTo", {}, 130,805);
-export const Sidebar_1 = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_kp31MGGaR", {}, 130,900);
+export const Sidebar = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_kp31MGGaR", {}, 130,900);
 
 export const colors = Object.freeze({
     /** #FFFFFF */
