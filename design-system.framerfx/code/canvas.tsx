d76a27a60762c00f7354580284d5aa7b6e63b1ae
Hope Tambala <hopetambala@outlook.com>
2020-02-21 00:02:53 -0500

    chore: fix apex-charts and coloring of sidebar

--- design-system.framerfx/code/canvas.tsx
+++ design-system.framerfx/code/canvas.tsx
@@ -1,8 +1,8 @@
 // WARNING: this file is auto generated, any changes will be lost
 import { createDesignComponent, CanvasStore } from "framer"
 const canvas = CanvasStore.shared(); // CANVAS_DATA;
-export const Header = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_NfKaLwHrx", {}, 1440,70);
-export const Left_menu = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_N7SPOhiTo", {}, 130,830);
+export const Menu = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_N7SPOhiTo", {}, 130,805);
+export const Sidebar_1 = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_kp31MGGaR", {}, 130,900);
 
 export const colors = Object.freeze({
     /** #FFFFFF */
